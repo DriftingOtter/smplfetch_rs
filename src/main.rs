fbf0beff3ba2549f7f6435bcc935a701b3d76975
Daksh <dakshkaul7790@gmail.com>
2024-05-15 23:30:09 +0530

    Created Git repo for rust rewrite

---
+++ src/main.rs
@@ -0,0 +1,139 @@
+use chrono;
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+use termcolor::{Color, ColorSpec, StandardStream, WriteColor};
+
+
+fn get_username() -> String {
+    return whoami::username();
+}
+
+
+fn get_distrobution_name() -> String {
+    return whoami::distro();
+}
+
+
+fn get_cpu_architecture() -> whoami::Arch {
+    return whoami::arch();
+}
+
+
+fn get_current_time() -> String {
+    return chrono::Local::now().time().format("%H:%M").to_string();
+}
+
+
+fn get_memory_useage() -> String { 
+    let sys = sysinfo::System::new_all();
+
+    let mut avalible_memory = sys.free_memory() as u64;
+    let mut used_memory     = sys.used_memory() as u64;
+
+    avalible_memory = avalible_memory / (1024 * 1024 * 1024);
+    used_memory     = used_memory     / (1024 * 1024 * 1024);
+
+    avalible_memory.to_string();
+    used_memory.to_string();
+
+    let memory_useage = used_memory.to_string() + "/" + avalible_memory.to_string().as_str() + " GB";
+
+    return memory_useage;
+}
+
+
+fn get_battery_percentage() -> String {
+  let paths = [
+      Path::new("/sys/class/power_supply/BAT0/capacity"), 
+      Path::new("/sys/class/power_supply/BAT1/capacity")
+  ];
+
+  for path in paths.iter() {
+      if let Ok(file) = File::open(path) {
+
+          let mut reader = BufReader::new(file);
+          let mut line = String::new();
+
+          match reader.read_line(&mut line) {
+
+              Ok(bytes_read) => {
+
+                  if bytes_read > 0 {
+                      line.pop();
+                      return format!("{}%", line.parse::<u8>().unwrap_or(0));
+                  }
+
+              },
+              Err(err) => println!("Error reading file: {}", err),
+          }
+      }
+  }
+  return "N/A".to_string();
+}
+
+
+fn generate_color_strip() {
+    let colored_block = "██";
+
+    let mut stdout = StandardStream::stdout(termcolor::ColorChoice::Always);
+
+    let colors = [
+        (Color::Black, "Black"),
+        (Color::Red, "Red"),
+        (Color::Green, "Green"),
+        (Color::Yellow, "Yellow"),
+        (Color::Blue, "Blue"),
+        (Color::Magenta, "Magenta"),
+        (Color::Cyan, "Cyan"),
+        (Color::White, "White"),
+    ];
+
+    let mut color_spec = ColorSpec::new();
+
+    for (color, _color_name) in &colors {
+        color_spec.set_fg(Some(*color));        
+        stdout.set_color(&color_spec).unwrap();
+
+        print!("{}", colored_block);
+        
+        stdout.reset().unwrap();
+    }
+    println!("");
+}
+
+
+fn get_color_strip() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        eprintln!("Invalid command-line argument. Please use the '-cs' flag to generate the color strip.");
+        return;
+    }
+
+    if args.len() == 2 && (args[1] == "-cs" || args[1] == "--color-strip") {
+        generate_color_strip();
+    }
+}
+
+
+fn main() {
+
+    println!("⎧                          ⎫");
+    println!("⎮          ⎮{}⎮         ⎮", get_current_time());
+    println!("⎩                          ⎭");
+
+
+    println!("User: {}",   get_username());
+    println!("Dist: {}",   get_distrobution_name());
+    println!("Krnl: {}",   get_cpu_architecture());
+
+
+    println!("Memory: {}", get_memory_useage());
+    println!("Battry: {}", get_battery_percentage());
+
+
+    get_color_strip();
+
+}
